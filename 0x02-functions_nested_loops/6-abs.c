e3ec1f9bcf68cbc26bee9f00f861fae7de3ed201
dukenyabuti <dukenyabuti@gmail.com>
2022-03-16 16:47:03 -0700

    prints thevalue of x

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs -check main
+ * @x: An integer input
+ * Description: it computes value of integer
+ * Return: ABsolute value of x
+ */
+int _abs(int x)
+{
+
+	if (x >= 0)
+		return (x);
+	else
+		return (x * -1);
+}
