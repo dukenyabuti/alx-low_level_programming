128bb5de7d9474a79773bfe65ec7cfced9a41203
dukenyabuti <dukenyabuti@gmail.com>
2022-03-18 18:43:00 -0700

    triangles

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,7 +14,7 @@ void print_diagonal(int n);
 void print_square(int size);
 int _putchar(char c);
 void print_number(int n);
-void print_traiangle( int size);
+void print_triangle( int size);
 void print_number(int n);
 
 #endif /* MAIN_H */
