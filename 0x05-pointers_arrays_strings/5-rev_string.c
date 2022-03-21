e97306d96feb20398c22dcecc4d765a33c920a17
dukenyabuti <dukenyabuti@gmail.com>
2022-03-21 16:59:46 -0700

    arrays, pointers and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - this can  reverse a string
+ * @s: an input string
+ *
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char aux;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len--)
+	{
+		aux = s[i];
+		s[i++] = s[len];
+		s[len] = aux;
+	}
+}
